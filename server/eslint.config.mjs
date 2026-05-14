0eabd7a9172b8cb715bed36109efcfb9c89e97ce
alpading <dnrwjd0116@naver.com>
2026-05-14 23:11:19 +0900

    chore: add ESLint and Prettier configuration

---
+++ server/eslint.config.mjs
@@ -0,0 +1,17 @@
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist/**', 'node_modules/**'] },
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+);
